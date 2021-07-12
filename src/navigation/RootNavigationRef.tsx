27af2dabc92a05385676bddf05efc3fc76f721bd
Sahej Maharjan <sahej@bottle.com.np>
2021-07-12 21:28:07 +0545

    added screen stack functions in RootNavigationRef

--- src/navigation/RootNavigationRef.tsx
+++ src/navigation/RootNavigationRef.tsx
@@ -1,5 +1,6 @@
 import {NavigationContainerRef} from '@react-navigation/core';
 import * as React from 'react';
+import {StackActions} from '@react-navigation/native';
 
 export const isReadyRef: React.MutableRefObject<boolean | null> =
   React.createRef();
@@ -17,11 +18,47 @@ export function navigate(name: String, params: Object) {
     // You can ignore this, or add these actions to a queue you can call later
   }
 }
+
+export function replace(name: string, params: Object = {}) {
+  if (isReadyRef.current && navigationRef.current) {
+    // Perform navigation if the app has mounted
+    //@ts-ignore
+    // console.log('replace', name, params);
+
+    navigationRef.current?.dispatch(StackActions.replace(name, params));
+  } else {
+    // You can decide what to do if the app hasn't mounted
+    // You can ignore this, or add these actions to a queue you can call later
+  }
+}
+
 export function goBack() {
   if (isReadyRef.current && navigationRef.current) {
     // Perform navigation if the app has mounted
     //@ts-ignore
     navigationRef.current.goBack();
+  } else {
+    // You can decide what to do if the app hasn't mounted
+    // You can ignore this, or add these actions to a queue you can call later
+  }
+}
+export function stackPop(index: number = 1) {
+  if (isReadyRef.current && navigationRef.current) {
+    // Perform navigation if the app has mounted
+    //@ts-ignore
+    navigationRef.current?.dispatch(StackActions.pop(index));
+
+    // navigationRef.current?.dispatch(StackActions.pop(index));
+  } else {
+    // You can decide what to do if the app hasn't mounted
+    // You can ignore this, or add these actions to a queue you can call later
+  }
+}
+export function currentRoute() {
+  if (isReadyRef.current && navigationRef.current) {
+    // Perform navigation if the app has mounted
+    //@ts-ignore
+    return navigationRef.current.getCurrentRoute();
   } else {
     // You can decide what to do if the app hasn't mounted
     // You can ignore this, or add these actions to a queue you can call later
